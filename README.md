a2cae59b8325a391e426194ded5a91d36889845e
Yamaguya <alextzov@outlook.com>
2024-05-19 15:34:49 +0300

    Restructured

--- README.md
+++
@@ -1,8 +0,0 @@
-# Chartl
-
-Chartl is a web app that allows users to organize albums and singles into concise, customizable charts.
-
-
-## Usage
-
-Select the type of media you're looking to chart and search for their title or creator. Click on the image you wish to add to the chart and then on the position you want to place it.
--- server/server.js
+++ server/server.js
@@ -1,11 +1,21 @@
-const cors = require('cors')
-const express = require('express')
-var app = express()
-app.use(cors())
+const cors = require('cors');
+const express = require('express');
+
+// Load environment variables
+require('dotenv').config(); 
+
+var app = express();
+
+app.use(cors());
 app.options('*', cors());
 
-app.get("/api", (req, res) => {
-    res.json({"API_KEY": "c1cdfe36b37e79fa24ca83d862a9dcaf"})
-})
+const API_KEY = process.env.API_KEY || "default_api_key";
 
-app.listen(5000, () => {console.log("Server started at 5000")})
+// Endpoint to provide API_KEY securely
+app.get("/api", (req, res) => {
+    res.json({"API_KEY": API_KEY})
+});
+
+app.listen(5000, () => {
+    console.log("Server started at 5000")
+});
